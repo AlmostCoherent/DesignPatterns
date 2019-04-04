44d0ac0e43a3044595952db67d9e235acfd1b416
Ben Harman <mrb.harman@gmail.com>
2019-04-04 14:18:00 +0100

    Added Start of builder and composite

---
+++ DesignPatterns.Tests/BuilderPatternTests.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns.BuilderPattern;
+using Xunit;
+
+namespace DesignPatterns.Tests
+{
+	public class BuilderPatternTests
+	{
+		[Fact]
+		public void CheckBuilder()
+		{
+			Builder builder = new Builder();
+
+			Assert.False(false);
+		}
+	}
+}
---
+++ DesignPatterns.Tests/CompositePatternTests.cs
@@ -0,0 +1,15 @@
+﻿using DesignPatterns.CompositePattern;
+using Xunit;
+
+namespace DesignPatterns.Tests
+{
+	public class CompositePatternTests
+	{
+		[Fact]
+		public void TestCompositeObject()
+		{
+			Composite composite = new Composite();
+			Assert.True(false);
+		}
+	}
+}
